4fb6a5e2bfb2f1f2ceef675074026123695333ae
clcamou <clcamou@gmail.com>
2020-04-09 00:22:18 -0700

    fixing broken code

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        Sign Up
+      </a>
+      <a className="navbar-brand" href="/">
+        Login In
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
